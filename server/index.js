923332a91f5b60eae03ec421889c37d1f7010014
Ian Chebii <ianchebii19@gmail.com>
2025-03-06 11:58:07 +0300

    vercel

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://ecommercestore-53iuc3waa-ian-chebiis-projects.vercel.app/",
+    origin: "https://ecommercestore-chi.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
