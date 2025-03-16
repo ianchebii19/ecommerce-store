ba66a6dce6df76752bd2f727ec2933182065aba7
Deborah Chebii <chebiideborah@gmail.com>
2025-03-16 15:29:03 +0300

    api

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  "https://ramon-store.vercel.app/auth/login",
+  "https://ramon-store.vercel.app",
 ];
 
 app.use(
